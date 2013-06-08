45672954ddfbe3f0a91b3f1866db9f39669fe064
Greg Fitzgerald <greg@gregf.org>
2013-06-07 23:08:11 -0400

    Add recipe description to metadata

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,8 @@ description      'Installs/Configures prosody'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
+recipe 'prosody', 'Installs sudo and configures prosody'
+
 %w{ apt }.each do |dep|
   depends dep
 end
