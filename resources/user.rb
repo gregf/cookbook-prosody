f940ecee6c78cdc74d899bbb478f5a817e991922
Greg Fitzgerald <greg@gregf.org>
2013-06-16 23:26:52 -0400

    Use [] instead of Array.new

--- resources/user.rb
+++ resources/user.rb
@@ -4,4 +4,4 @@ default_action :create
 
 attribute :username, :kind_of => String, :name_attribute => true
 attribute :password, :kind_of => String, :required => true, :default => nil
-attribute :vhosts, :kind_of => Array, :required => true, :default => Array.new
+attribute :vhosts, :kind_of => Array, :required => true, :default => []
