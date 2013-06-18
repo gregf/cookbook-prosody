6dffaa28636d8c6a06225da499930121e59d1071
Greg Fitzgerald <greg@gregf.org>
2013-06-18 19:32:23 -0400

    Add optional ipv6 support.
    This is disabled by default. If enough slap me, I'll happily turn it
    on. I suspect most servers are not ipv6 enabled still, and I'm
    afraid enabling it by default could cause issues in those cases.
    
    If enabled it installs lua-socket from the prosody apt repository.
    
    See http://blog.prosody.im/prosody-0-9-0rc3-available-for-testing/
    for more information

--- recipes/default.rb
+++ recipes/default.rb
@@ -29,6 +29,7 @@ end
 
 package node['prosody']['luasec_package'] if node['prosody']['s2s_secure_auth']
 package node['prosody']['libevent_package'] if node['prosody']['use_libevent']
+package node['prosody']['lua_socket_package'] if node['prosody']['ipv6']
 
 package node['prosody']['package'] do
   action :install
